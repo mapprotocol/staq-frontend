df257c4c0a4163c8e5a97d1408d2aec8c182c934
cokes123 <sixuwang@sixudeMacBook-Pro.local>
2024-03-04 12:45:07 +0800

    feat: homepage fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,49 +1,138 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './index.module.css'
 import Image from 'next/image'
 import { Router, useRouter } from 'next/router';
+import { useAccount } from 'wagmi';
 
 
 
 const Header = () => {
+  const { address, isConnected, isConnecting } = useAccount();
+  const [showSelect, setShowSelect] = useState(false)
   const router = useRouter()
+  useEffect(() => {
+    console.log(isConnected)
+
+  }, [])
   return (
     <div className={styles.header}>
-      <div onClick={() => {
-        router.push('/')
-      }}>
-        <Image
-          style={{
-            cursor: 'pointer'
-          }}
-          height={32}
-          width={98}
-          src="/images/logo.png"
-          alt="map" />
-      </div>
-      <div className={styles.line}></div>
-      <div className={styles.item} onClick={() => {
-        router.push('/stake')
-      }}>
-        <Image
-          height={16}
-          width={16}
-          src="/icons/stake.svg"
-          alt="map" />
-        {'Stake'}
-      </div>
-      <div className={styles.item} onClick={() => {
-        router.push('/withdraw')
-      }}>
-        <Image
-          height={16}
-          width={16}
-          src="/icons/withdraw.svg"
-          alt="map" />
-        {'Withdraw'}
-      </div>
+      <div className={styles.headerLeft}>
+        <div onClick={() => {
+          router.push('/')
+        }}>
+          <Image
+            style={{
+              cursor: 'pointer'
+            }}
+            height={32}
+            width={98}
+            src="/images/logo.png"
+            alt="map" />
+        </div>
+        <div className={styles.line}></div>
+        <div className={styles.item} onClick={() => {
+          router.push('/stake')
+        }}>
+          <Image
+            height={16}
+            width={16}
+            src="/icons/stake.svg"
+            alt="map" />
+          {'Stake'}
+        </div>
+        <div className={styles.item} onClick={() => {
+          router.push('/withdraw')
+        }}>
+          <Image
+            height={16}
+            width={16}
+            src="/icons/withdraw.svg"
+            alt="map" />
+          {'Withdraw'}
+        </div>
 
-    </div>
+      </div>
+      <div className={styles.headerRight}>
+       {(router.route !== "/" && isConnected) && <div onClick={() => {
+          setShowSelect(!showSelect)
+        }} className={styles.selectNetwork}>
+          <div className={styles.chainLogo}>
+            <Image
+              style={{
+                cursor: 'pointer'
+              }}
+              height={30}
+              width={30}
+              src="/images/chain0.png"
+              alt="map" /></div>
+          <div className={styles.chainName}>
+            {"MAP Protocol"}
+            <Image
+              style={{
+                marginLeft: "12px"
+              }}
+              height={10}
+              width={10}
+              src={`/images/${showSelect ? "up" : "down"}.svg`}
+              alt="map" />
+          </div>
+        </div>}
+
+        {showSelect && <div className={styles.networks}>
+          <div className={styles.clainItem}>
+            <div className={styles.chainLogo}>
+              <Image
+                style={{
+                  cursor: 'pointer'
+                }}
+                height={30}
+                width={30}
+                src="/images/chain1.png"
+                alt="map" /></div>
+            <div className={styles.chainName}>
+              {"Merlin Chain"}
+              <div className={styles.coming}>{"coming soon"}</div>
+
+            </div>
+          </div>
+
+          <div className={styles.clainItem}>
+
+            <div className={styles.chainLogo}>
+              <Image
+                style={{
+                  cursor: 'pointer'
+                }}
+                height={30}
+                width={30}
+                src="/images/chain2.png"
+                alt="map" /></div>
+            <div className={styles.chainName}>
+              {"Conflux"}
+              <div className={styles.coming}>{"coming soon"}</div>
+            </div>
+          </div>
+          <div className={styles.clainItem}>
+
+            <div className={styles.chainLogo}>
+              <Image
+                style={{
+                  cursor: 'pointer'
+                }}
+                height={30}
+                width={30}
+                src="/images/chain3.png"
+                alt="map" /></div>
+            <div className={styles.chainName}>
+              {"B² Network"}
+              <div className={styles.coming}>{"coming soon"}</div>
+
+            </div>
+          </div>
+
+        </div>}
+      </div>
+    </div >
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,13 +41,48 @@ const Home: NextPage = () => {
         }} className={styles.enterButton}>
           {"Staking Now"}
         </div>
+
+        <div className={styles.datas}>
+          <div className={styles.data}>
+            <div className={styles.dataTitle}>
+              {'Total Staked Tokens'}
+            </div>
+            <div className={styles.dataDesc}>
+              {'4044945949499'}
+            </div>
+          </div>
+          <div className={styles.data}>
+            <div className={styles.dataTitle}>
+              {'Total Rewards Paid'}
+            </div>
+            <div className={styles.dataDesc}>
+              {'54959595'}
+            </div>
+          </div>
+          <div className={styles.data}>
+            <div className={styles.dataTitle}>
+              {'Stakers'}
+            </div>
+            <div className={styles.dataDesc}>
+              {'394949'}
+            </div>
+          </div>
+          <div className={styles.data}>
+            <div className={styles.dataTitle}>
+              {'L2 Chains'}
+            </div>
+            <div className={styles.dataDesc}>
+              {'15'}
+            </div>
+          </div>
+        </div>
       </div>
-      {/* <div className={styles.body}>
+      <div className={styles.bodyItem}>
         <div className={styles.left}>
-          <div>
+          <div className={styles.bodyTitle}>
             {"staQ解决什么问题？"}
           </div>
-          <div>
+          <div className={styles.bodyDesc}>
             {"随着比特币生态的蓬勃发展，用户质押进比特币二层主网节点的代币，虽可享受收益，但受主网的安全机制限制，提出代币一般需要等待很久的时间，让用户流动性需求受阻。"}
           </div>
         </div>
@@ -60,9 +95,174 @@ const Home: NextPage = () => {
             src="/images/1.png"
             alt="map" />
         </div>
+
+      </div>
+      <div className={styles.bodyItem} style={{
+        backgroundColor: '#F7F7F7'
+      }}>
+        <div className={styles.right}>
+          <Image
+            fill
+            style={{
+              objectFit: "contain",
+            }}
+            src="/images/2.png"
+            alt="map" />
+        </div>
+        <div className={styles.left}>
+          <div className={styles.bodyTitle}>
+            {"StaQ如何解决？"}
+          </div>
+          <div className={styles.bodyDesc}>
+            {"StaQ在各比特币Layer2搭建主网节点，用户只需将自己的主网代币，委托质押给StaQ的节点，StaQ便会1:1给到用户一个映射代币，该映射代币和主网代币在defi中保持约1：1的兑换比例，并且用户可以自动获得利息收入。"}
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.user}>
+
+        <div className={styles.userTitle}>{"用户如何操作"}</div>
+        <div className={styles.steps}>
+
+          <div className={styles.step}>
+            <div className={styles.stepImage}><Image
+              fill
+              style={{
+                objectFit: "contain",
+              }}
+              src="/images/step1.png"
+              alt="map" /></div>
+            <div className={styles.stepTitle}>{"STEP 1"}</div>
+            <div className={styles.stepDesc}>{"用户委托质押进StaQ在比特币L2主网节点"}</div>
+
+          </div>
+          <div className={styles.step}>
+            <div className={styles.stepImage}><Image
+              fill
+              style={{
+                objectFit: "contain",
+              }}
+              src="/images/step2.png"
+              alt="map" /></div>
+            <div className={styles.stepTitle}>{"STEP 2"}</div>
+            <div className={styles.stepDesc}>{"收到ST Tokens 以及奖励"}</div>
+
+          </div>
+          <div className={styles.step}>
+            <div className={styles.stepImage}><Image
+              fill
+              style={{
+                objectFit: "contain",
+              }}
+              src="/images/step3.png"
+              alt="map" /></div>
+            <div className={styles.stepTitle}>{"STEP 3"}</div>
+            <div className={styles.stepDesc}>{"将ST Tokens用到各种defi中享受更多的收益"}</div>
+
+          </div>
+
+        </div>
+      </div>
+
+
+      <div className={styles.bodyItem}
+        style={{
+          background: "linear-gradient(90deg, #EAF3FF 0%, #F8F4FF 100%), #F7F7F7"
+        }}>
+        <div className={styles.left}>
+          <div className={styles.bodyTitle}>
+            {"StaQ部署于繁荣的比特币L2网络"}
+          </div>
+          <div className={styles.bodyDesc}>
+            {"包括MAP Protocol、Merlin、BEVM、B Square 等，这意味着StaQ是服务所有比特币生态二层的质押流动性提供商。"}
+          </div>
+        </div>
+        <div className={styles.right}>
+          <Image
+            fill
+            style={{
+              objectFit: "contain",
+            }}
+            src="/images/3.png"
+            alt="map" />
+        </div>
+      </div>
+      <div className={styles.bodyItem} style={{
+        background: "linear-gradient(90deg, #FFF2E3 3.73%, #DAEAFF 94.08%)"
+      }}>
+        <div className={styles.right}>
+          <Image
+            fill
+            style={{
+              objectFit: "contain",
+            }}
+            src="/images/4.png"
+            alt="map" />
+        </div>
+        <div className={styles.left}>
+          <div className={styles.bodyTitle}>
+            {"StaQ 通过 MAP Protocol 进行流动性互通"}
+          </div>
+          <div className={styles.bodyDesc}>
+            {"不同主网的Stake Tokens无法互相拥有流动性，StaQ通过比特币二层互操作性入口MAP Protocol，让各个比特币L2主网的staking tokens，实现流动性互换。"}
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.defi}>
+        <div className={styles.defiTitle}>
+          {"哪些defi支持StaQ？"}
+        </div>
+        <div className={styles.defiDesc}>
+          {"StaQ的stake token被众多比特币L2的defi支持流动性及收益奖励，并在不断增加。"}
+        </div>
+
+
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <div className={styles.cardImage}>
+              <Image
+                fill
+                style={{
+                  objectFit: "contain",
+                }}
+                src="/images/7.png"
+                alt="map" />
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.cardImage}>
+              <Image
+                fill
+                style={{
+                  objectFit: "contain",
+                }}
+                src="/images/8.png"
+                alt="map" />
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.cardImage}>
+              <Image
+                fill
+                style={{
+                  objectFit: "contain",
+                }}
+                src="/images/9.png"
+                alt="map" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className={styles.audit}>
+        <div className={styles.auditTitle}>
+          {"StaQ审计报告"}
+        </div>
+        <div>
+
+        </div>
       </div> */}
-
-
     </>
   );
 };
